ed94db586e76174ec30ad82fe6d348422a237cca
EwoutKa <91463387+EwoutKa@users.noreply.github.com>
2022-01-27 17:13:37 +0100

    Add files via upload

---
+++ common1.py
@@ -0,0 +1,109 @@
+import sys
+
+class Place:
+  pass
+
+class Trolley:
+  pass
+
+class Route:
+  pass
+
+class Route2use:
+  pass
+
+class Instance:
+
+  def __init__(self, fileName):
+    f = open(fileName, 'r')
+    data = f.read().split()
+    i = 0
+    self.places = {}
+    self.dist = {}
+    self.hourDistances = {}
+    self.trolleys = {}
+    self.start = 9999999
+    self.end = -9999999
+    while i < len(data):
+      if data[i] == 'T':
+        self.tickHours = float(data[i+1])
+        self.timeShift = float(data[i+2])
+        self.crossTicks = int(data[i+3])
+        i += 4
+      elif data[i] == 'p':
+        place = Place()
+        place.name = data[i+2]
+        place.lattitude = float(data[i+3])
+        place.longitude = float(data[i+4])
+        place.isTarget = data[i+5] == '1'
+        place.isCross = data[i+6] == '1'
+        place.inCapacity = int(data[i+7])
+        place.outCapacity = int(data[i+8])
+        place.inAmount = 0
+        place.outAmount = 0
+        self.places[int(data[i+1])] = place
+        i += 9
+      elif data[i] == 'd':
+        self.dist[int(data[i+1]),int(data[i+2])] = int(data[i+3])
+        self.hourDistances[int(data[i+1]),int(data[i+2])] = float(data[i+4])
+        i += 5
+      elif data[i] == 't':
+        trolley = Trolley()
+        trolley.origin = int(data[i+2])
+        trolley.destination = int(data[i+3])
+        trolley.release = int(data[i+4])
+        trolley.deadline = int(data[i+5])
+        self.trolleys[int(data[i+1])] = trolley
+        i += 6
+        if trolley.release < self.start:
+          self.start = trolley.release
+        if trolley.deadline > self.end:
+          self.end = trolley.deadline
+        trolley.position = -1   # keeps track of the position of the trolley
+      else:
+        sys.stderr.write(f'Ignoring token <{data[i]}>.\n')
+        i += 1
+
+  def writeRoutes(self, routes, fileStream=sys.stdout):
+    for key,routeSet in routes.items():
+      for route in routeSet:
+        assert key[0] == route[0] and key[1] == route[-1]
+        fileStream.write(f'{key[0]} {key[1]} {len(route)-2}' + ' '.join(map(str, route[1:-1])) + '\n')
+    fileStream.flush()
+
+  def readRoutes(self, fileName):
+    f = open(fileName, 'r')
+    data = f.read().split()
+    i = 0
+    self.routes = {}
+    while i < len(data):
+      route = Route()
+      route.origin = int(data[i])
+      route.destination = int(data[i+1])
+      route.numberOfIntermediates = int(data[i+2])
+      intermediatePlaces = ''
+      for j in range(route.numberOfIntermediates):
+        intermediatePlaces += data[i+3+j]
+      route.itermediatePlaces = intermediatePlaces
+      self.routes[int(data[i]),int(data[i+1])] = route
+      i += 3 + route.numberOfIntermediates
+
+  def readRoutes2use(self, fileName):
+    f = open(fileName, 'r')
+    data = f.read().split()
+    i = 0
+    self.routes2use = {}
+    while i < len(data):
+      if data[i] == 'x':
+        route2use = Route()
+        route2use.origin = int(data[i+1])
+        route2use.destination = int(data[i+2])
+        self.routes2use[int(data[i+1]),int(data[i+2])] = route2use
+        i += 3 
+      elif data[i] == 'y':
+        i += 4
+        pass
+      else:
+        sys.stderr.write(f'Ignoring token <{data[i]}>.\n')
+        i += 1
+        
---
+++ makeMap.py
@@ -0,0 +1,59 @@
+import sys
+from common1 import *
+import matplotlib.pyplot as plt
+#from mpl_toolkits.basemap import Basemap
+
+
+if len(sys.argv) <= 2:
+  sys.stderr.write(f'Usage: {sys.argv[0]} INSTANCE-FILE ROUTES-FILE\nCreates a map of the routes.\n')
+  sys.exit(1)
+
+instance = Instance(sys.argv[1])
+sys.stderr.write(f'Read instance with {len(instance.places)} places, {len(instance.trolleys)} trolleys and time range [{instance.start},{instance.end}].\n')
+instance.readRoutes2use(sys.argv[2])
+
+lat_min = 999999
+lat_max = -999999
+lon_min = 999999
+lon_max = -999999
+for t,data in instance.places.items():
+    if data.lattitude < lat_min:
+        lat_min = data.lattitude
+    if data.lattitude > lat_max:
+        lat_max = data.lattitude
+    if data.longitude < lon_min:
+        lon_min = data.longitude
+    if data.longitude > lon_max:
+        lon_max = data.longitude
+
+import matplotlib.pyplot as plt
+
+for t,data in instance.routes2use.items(): 
+    #print(f'route from {instance.places[data.origin].longitude}, {instance.places[data.origin].lattitude} to {instance.places[data.destination].longitude}, {instance.places[data.destination].lattitude}')
+    lat = [instance.places[data.origin].lattitude, instance.places[data.destination].lattitude] 
+    lon = [instance.places[data.origin].longitude, instance.places[data.destination].longitude] 
+    
+    plt.plot(lat, lon,  marker = 'o')
+
+plt.show()
+
+
+
+"""    
+     
+m = Basemap(projection = 'merc', llcrnrlat=lat_min - 2,urcrnrlat=lat_max + 2, llcrnrlon=lon_min - 2, urcrnrlon=lon_max + 2, lat_ts=40, resolution='l')
+
+for t,data in instance.routes2use.items(): 
+    #print(f'route from {instance.places[data.origin].longitude}, {instance.places[data.origin].lattitude} to {instance.places[data.destination].longitude}, {instance.places[data.destination].lattitude}')
+    lat = [instance.places[data.origin].lattitude, instance.places[data.destination].lattitude] 
+    lon = [instance.places[data.origin].longitude, instance.places[data.destination].longitude] 
+    x, y = m(lon, lat)
+    m.plot(x, y, marker=None, markersize=5, linewidth=1) 
+
+m.drawcoastlines()
+m.fillcontinents(color='white')
+m.drawmapboundary(fill_color='white')
+m.drawcountries(color='black')
+plt.title("#wedgez")
+plt.show() 
+"""
