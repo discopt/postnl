48a0a0c7c319773105f969b99e1d97a6556f2e48
Matthias Walter <matthias@matthiaswalter.org>
2022-03-14 13:45:12 +0100

    attempts

--- refactor/mip.py
+++ refactor/mip.py
@@ -5,8 +5,11 @@ from common import *
 def printUsage(errorMessage=None):
   if errorMessage is not None:
     print(f'Error: {errorMessage}')
-  print(f'Usage: {sys.argv[0]} NETWORK TICKHOURS TICKZERO TROLLEYS')
-  print('Solves the MIP for network and TROLLEYS, discretizing TICKHOURS hours with offset TICKZERO hours.')
+  print(f'Usage: {sys.argv[0]} <network file name> <tickhours> <tickzero> <trolleys file name> [OPTIONS...]')
+  print('Solves the MIP for network and trolleys, discretizing tickhours hours with offset tickzero hours.')
+  print('Options:')
+  print('  -oT FILE  Write used trucks to <FILE>.')
+  print('  -rT FILE  Read used trucks from <FILE> and use routes.')
   sys.exit(1)
 
 if len(sys.argv) < 5:
@@ -16,6 +19,21 @@ network = Network(sys.argv[1])
 trolleys = network.readTrolleys(sys.argv[4])
 tickHours = float(sys.argv[2])
 tickZero = float(sys.argv[3])
+usedTrucksOutputFileName = None
+usedTruckRoutesFileName = None
+a = 5
+while a < len(sys.argv):
+  arg = sys.argv[a]
+  if arg == '-oT' and a+1 < len(sys.argv):
+    usedTrucksOutputFileName = sys.argv[a+1]
+    a += 1
+  elif arg == '-rT' and a+1 < len(sys.argv):
+    usedTruckRoutesFileName = sys.argv[a+1]
+    a += 1
+  else:
+    printUsage(f'Unprocessed argument <{arg}>.')
+  a += 1
+
 
 print(f'Read instance with {len(network.locations)} locations and {len(trolleys)} trolleys.')
 
@@ -45,16 +63,29 @@ class MIP:
     self._varNotDelivered = {}
     self._varNotProduced = {}
     self._varExtendedCapacity = {}
+    self._varExtraDocks = {}
     self._undeliveredPenalty = 1e4
     self._extenedCapacityCost = 1e4
+    self._extraDockPenalty = 1e4
 
   def scanTrolleys(self, trolleys):
+    deliverableTrolleys = []
+    first = True
     for t in trolleys:
       tick = network.trolleyReleaseTick(t)
+      if self.network.trolleyReleaseTick(t) + self.network.travelTicks(t.source, t.commodity[0]) > self.network.deadlineTick(t.commodity):
+        if first:
+          print(f'Example for a trolley that cannot be delivered for time reasons: {t.source}<{self.network.name(t.source)}> -> {t.commodity[0]}<{self.network.name(t.commodity[0])}> within [{t.release},{self.network.deadline(t.commodity)}], but distance is {self.network.distance(t.source, t.commodity[0])}')
+          print(f'Ticks are {self.network.trolleyReleaseTick(t)} + {self.network.travelTicks(t.source, t.commodity[0])} > {self.network.deadlineTick(t.commodity)}')
+          first = False
+        continue
+
+      deliverableTrolleys.append(t)
       if tick < self._minTick:
         self._minTick = tick
       if tick > self._maxTick:
         self._maxTick = tick
+    return deliverableTrolleys
 
   @property
   def network(self):
@@ -72,14 +103,30 @@ class MIP:
   def arcs(self):
     return self._arcs
 
-  def createTruckVars(self):
+  def createTruckVars(self, free=False):
     print('Creating truck variables.')
     self._varTruck = {}
     for (i,j) in self.arcs:
       for t in self.ticks:
         if t + self.network.travelTicks(i,j) <= max(self.ticks):
-          obj = self.network.distance(i,j) + 1.0e-3
+          obj = self.network.distance(i,j)
+          if free:
+            obj = 0.0
           self._varTruck[i,j,t] = self._model.addVar(name=f'x#{i}#{j}#{t}', vtype=GRB.INTEGER, obj=obj)
+    self._model.update()
+
+  def createExtraDocksVars(self):
+    print('Creating extra docks variables.')
+    self._varExtraDocks = {}
+    for i in self.nodes:
+      self._varExtraDocks[i] = self._model.addVar(name=f'extradocks#{i}', obj=self._extraDockPenalty)
+    self._model.update()
+
+  def createTruckInfinite(self):
+    self._varTruck = {}
+    for (i,j) in self.arcs:
+      for t in self.ticks:
+        self._varTruck[i,j,t] = 1000
     self._model.update()
 
   def createFlowVars(self):
@@ -102,12 +149,14 @@ class MIP:
           obj = 1.0e5 if t == max(self.ticks) else 0.0
           ub = 0 if t == max(self.ticks) else 1
           self._varInventory[i,t,target,shift] = self._model.addVar(name=f'z#{i}#{t}#{target}#{shift}', vtype=self._vtypeInventory, obj=obj, ub=ub)
+    self._model.update()
 
   def createNotDeliveredVars(self):
     print('Creating non-delivery variables.')
     self._varNotDelivered = {}
     for target,shift in self.network.commodities:
       self._varNotDelivered[target,shift] = self._model.addVar(name=f'notdeliver#{target}#{shift}', obj=self._undeliveredPenalty)
+    self._model.update()
 
   def createNotProducedVars(self):
     print('Creating non-production variables.')
@@ -124,6 +173,7 @@ class MIP:
         for target,shift in self.network.commodities:
           if (i,t,target,shift) in production:
             self._varNotProduced[i,t,target,shift] = self._model.addVar(name=f'notproduced#{i}#{t}#{target}#{shift}', obj=self._undeliveredPenalty)
+    self._model.update()
 
   def createExtendedCapacityVars(self):
     print('Creating extended-capacity variables.')
@@ -146,7 +196,10 @@ class MIP:
       for t in self.ticks:
         departing = quicksum( self._varTruck[i,j,t-eta] for j in self.nodes for eta in range(loadingTicks) if (i,j,t) in self._varTruck )
         arriving = quicksum( self._varTruck[j,i,t-self.network.travelTicks(j,i)+unloadingTicks-eta] for j in self.nodes for eta in range(unloadingTicks) if (j,i,t-self.network.travelTicks(j,i)+unloadingTicks-eta) in self._varTruck )
-        self._model.addConstr( departing + arriving <= self.network.numDocksPerTick(i), f'docking#{i}#{t}')
+        extraDocks = 0
+        if i in self._varExtraDocks:
+          extraDocks = self._varExtraDocks[i]
+        self._model.addConstr( departing + arriving <= self.network.numDocksPerTick(i) + extraDocks, f'docking#{i}#{t}')
 
   def createFlowBalanceConstraints(self, trolleys):
     print('Creating flow balance constraints.')
@@ -179,7 +232,7 @@ class MIP:
             produced = produced - self._varNotProduced[i,t,target,shift]
           self._model.addConstr( newInventory - oldInventory + outFlow - inFlow == produced - consumed, f'flow_balance#{i}#{t}#{target}#{shift}')
     if sumRhs != 0:
-      assert 'Total flow balance of network is nonzero!' is None
+      assert 'Total flow balance of network is nonzero!' == None
 
   def createSourceCapacityConstraints(self):
     print('Creating source capacity constraints.')
@@ -203,6 +256,15 @@ class MIP:
 
   def write(self, fileName):
     self._model.write(fileName)
+
+  def writeUsedTrucks(self, fileName):
+    f = open(fileName, 'w')
+    for (i,j) in self.arcs:
+      for t in self.ticks:
+        if t + self.network.travelTicks(i,j) <= max(self.ticks):
+          if self._varTruck[i,j,t].x > 0.5:
+            f.write(f'T {i} {j} {self.network.tickTime(t)}\n')
+    f.close()
 
   def printSolution(self):
     loadingTicks = self.network.loadingTicks
@@ -230,25 +292,68 @@ class MIP:
             print(f'Commodity {target}<{self.network.name(target)}>,{shift} has {round(self._varNotProduced[i,t,target,shift].x,0)} trolleys not produced in {i}<{self.network.name(i)}> at tick {t}.')
     print(f'Total driving distance is {totalDrivingDistance}.')
 
+requiredTrolleys = [ t for t in trolleys if t.source != t.commodity[0] ]
+print(f'{len(trolleys) - len(requiredTrolleys)} trolleys have origin = destination.')
+
 mip = MIP(network)
-mip.scanTrolleys(trolleys)
-
+deliverableTrolleys = mip.scanTrolleys(requiredTrolleys)
 print(f'Ticks are in range [{min(mip.ticks)},{max(mip.ticks)}].')
-
-mip.createTruckVars()
+print(f'{len(requiredTrolleys) - len(deliverableTrolleys)} trolleys cannot be delivered for time reasons.')
+mip.createTruckVars(True)
 mip.createFlowVars()
 mip.createInventoryVars()
-mip.createNotDeliveredVars()
-mip.createNotProducedVars()
-mip.createExtendedCapacityVars()
+mip.createExtraDocksVars()
+#mip.createNotDeliveredVars()
+#mip.createNotProducedVars()
+#mip.createExtendedCapacityVars()
 mip.createCapacityConstraints()
-mip.createDockingConstraints()
 mip.createSourceCapacityConstraints()
 mip.createTargetCapacityConstraints()
-mip.createFlowBalanceConstraints(trolleys[:len(trolleys)])
+mip.createFlowBalanceConstraints(deliverableTrolleys)
+mip.createDockingConstraints()
 
-mip.write('mip.lp')
 status = mip.optimize()
-mip.printSolution()
+
+if status != GRB.INFEASIBLE and status != GRB.INF_OR_UNBD and status != GRB.UNBOUNDED:
+  mip.printSolution()
+  if len(sys.argv) >= 6:
+    mip.writeUsedTrucks(sys.argv[5])
+
+
+#mip.write('mip.lp')
+
+#status = mip.optimize()
+#removedTrolleys = {}
+#for i in mip.nodes:
+#  for t in mip.ticks:
+#    for target,shift in mip.network.commodities:
+#      removed = int(round(mip._varNotProduced[i,t,target,shift].x)) if (i,t,target,shift) in mip._varNotProduced else 0
+#      if removed > 0:
+#        removedTrolleys[(i,t,target,shift)] = removedTrolleys.get((i,t,target,shift), 0) + removed
+#print(removedTrolleys)
+#print(len(removedTrolleys))
+#
+#sys.exit(0)
+
+#mip = MIP(network)
+#mip.scanTrolleys(trolleys)
+#
+#print(f'Ticks are in range [{min(mip.ticks)},{max(mip.ticks)}].')
+
+#mip.createTruckVars()
+#mip.createFlowVars()
+#mip.createInventoryVars()
+#mip.createNotDeliveredVars()
+#mip.createNotProducedVars()
+#mip.createExtendedCapacityVars()
+#mip.createCapacityConstraints()
+#mip.createDockingConstraints()
+#mip.createSourceCapacityConstraints()
+#mip.createTargetCapacityConstraints()
+#mip.createFlowBalanceConstraints(trolleys[:len(trolleys)])
+
+#mip.write('mip.lp')
+#status = mip.optimize()
+#mip.printSolution()
 
 
